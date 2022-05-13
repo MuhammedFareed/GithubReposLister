23718248c93def9604c7b053753496d34f5b1ae1
Muhammed Fareed <muhammed.fareed.94@gmail.com>
2022-05-14 00:33:21 +0200

    Creating list view controller

---
+++ GithupReposApp/ReposList/View/ReposListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ReposListViewController.swift
+//  GithupReposApp
+//
+//  Created by Muhammad Fareed on 14/05/2022.
+//
+
+import UIKit
+
+class ReposListViewController: UIViewController {
+
+    @IBOutlet private weak var listTableView: UITableView!
+    @IBOutlet weak var repoNambeLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
