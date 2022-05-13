e2104ed635914c259920da33c069f210555425cb
Muhammed Fareed <muhammed.fareed.94@gmail.com>
2022-05-14 00:48:32 +0200

    Creating protocol for list view

--- GithupReposApp/ReposList/View/ReposListViewController.swift
+++ GithupReposApp/ReposList/View/ReposListViewController.swift
@@ -7,14 +7,70 @@
 
 import UIKit
 
+protocol ReposListView: UIViewController {
+    func reloadListTableView()
+    func showLoading()
+    func stopLoading()
+    func showError(withMessage message: String)
+}
+
 class ReposListViewController: UIViewController {
 
     @IBOutlet private weak var listTableView: UITableView!
-    @IBOutlet weak var repoNambeLabel: UILabel!
+    
+    private var loadingView: UIView?
+    private var errorView: UIView?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    
+}
 
-        // Do any additional setup after loading the view.
+extension ReposListViewController: ReposListView {
+    func reloadListTableView() {
+        
+    }
+    
+    func showLoading() {
+        guard loadingView == nil else {
+            return
+        }
+        self.loadingView = UIView(frame: self.view.frame)
+        DispatchQueue.main.async {
+            self.loadingView?.translatesAutoresizingMaskIntoConstraints = false
+            self.loadingView?.backgroundColor = self.view.backgroundColor
+            self.view.addSubview(self.loadingView!)
+            let loadingIndicator = UIActivityIndicatorView()
+            loadingIndicator.style = .large
+            loadingIndicator.translatesAutoresizingMaskIntoConstraints = false
+            self.loadingView?.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+            self.loadingView?.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+            self.loadingView?.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+            self.loadingView?.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+            self.loadingView?.addSubview(loadingIndicator)
+            loadingIndicator.widthAnchor.constraint(equalToConstant: 200).isActive = true
+            loadingIndicator.heightAnchor.constraint(equalToConstant: 200).isActive = true
+            loadingIndicator.centerXAnchor.constraint(equalTo: self.loadingView!.centerXAnchor).isActive = true
+            loadingIndicator.centerYAnchor.constraint(equalTo: self.loadingView!.centerYAnchor).isActive = true
+            loadingIndicator.startAnimating()
+            self.view.bringSubviewToFront(self.loadingView!)
+        }
+        
+    }
+    
+    func stopLoading() {
+        guard loadingView != nil else {
+            return
+        }
+        DispatchQueue.main.async {
+            self.loadingView?.removeFromSuperview()
+        }
+    }
+    
+    func showError(withMessage message: String) {
+        
     }
 }
+
