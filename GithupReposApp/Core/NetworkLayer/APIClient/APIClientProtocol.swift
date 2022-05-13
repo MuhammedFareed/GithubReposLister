1a0de0f03d257eb439795a61a44c7a4567d0e5bf
Muhammed Fareed <muhammed.fareed.94@gmail.com>
2022-05-13 22:48:01 +0200

    Creating type aliases for success and failure closures

---
+++ GithupReposApp/Core/NetworkLayer/APIClient/APIClientProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  APIClientProtocol.swift
+//  GithupReposApp
+//
+//  Created by Muhammad Fareed on 13/05/2022.
+//
+
+import Foundation
+
+typealias NetworkSuccess = (Codable) -> Void
+typealias NetworkFailure = (Error) -> Void
+
+
