cdb54c2efe5575e8f5eecca9a46dbd318d10a536
Muhammed Fareed <muhammed.fareed.94@gmail.com>
2022-05-14 00:35:55 +0200

    Creating cell UI Model

---
+++ GithupReposApp/ReposList/Model/UIModels/RepoUIModel.swift
@@ -0,0 +1,20 @@
+//
+//  RepoUIModel.swift
+//  GithupReposApp
+//
+//  Created by Muhammad Fareed on 14/05/2022.
+//
+
+import Foundation
+
+protocol RepoUIModelProtocol {
+    var repoName: String { get set }
+    var authorName: String { get set }
+    var authorImageLink: String { get set }
+}
+
+struct RepoUIModel: RepoUIModelProtocol {
+    var repoName: String
+    var authorName: String
+    var authorImageLink: String
+}
