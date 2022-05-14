ba8fe4b20b74d15dd3f188a9af15803679656fbe
Muhammed Fareed <muhammed.fareed.94@gmail.com>
2022-05-14 02:40:35 +0200

    updating table view cell style

--- GithupReposApp/ReposList/View/Cell/RepoTableViewCell.swift
+++ GithupReposApp/ReposList/View/Cell/RepoTableViewCell.swift
@@ -17,6 +17,7 @@ class RepoTableViewCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         
+        setupStyles()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
