6b299174a2ed7727ef8dd55f03063e69bc868d38
Muhammed Fareed <muhammed.fareed.94@gmail.com>
2022-05-14 13:02:06 +0200

    Fixing issue in running tests cause of ui test target

---
+++ GithupReposAppUITests/GithupReposAppUITestsLaunchTests.swift
@@ -0,0 +1,19 @@
+//
+//  GithupReposAppUITestsLaunchTests.swift
+//  GithupReposAppUITests
+//
+//  Created by Muhammad Fareed on 13/05/2022.
+//
+
+import XCTest
+
+class GithupReposAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+}
